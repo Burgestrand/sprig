9a5ff04f84d7292d1be726846b5213b6cf5b67e7
Kim Burgestrand <kim@burgestrand.se>
2010-02-24 13:46:54 +0100

    Adds Sprig_Field_IP

---
+++ classes/sprig/field/ip.php
@@ -0,0 +1,32 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+    /**
+     * An IP-address sprig Field, stored in a 16-byte (unsigned) binary field
+     *
+     * @author      Kim Burgestrand
+     * @license     X11 License (“MIT”)
+     */
+    class Sprig_Field_Ip extends Sprig_Field_Char
+    {
+        public $min_length = 4; // IPv4
+        public $max_length = 16; // IPv6
+        public $default = NULL;
+        public $null = TRUE;
+        
+        public function value($value)
+        {
+            $value = parent::value($value);
+            
+            if (is_null($value))
+            {
+                return inet_pton(Request::$client_ip);
+            }
+            else
+            {
+                return Validate::ip($value) ? inet_pton($value) 
+                                            : inet_ntop($value);
+            }
+        }
+    }
+    
+/* End of file ip.php */
+/* Location: ./application/classes/sprig/field/ip.php */ 
