13935028909370b2900bef0b015b5a64b86b52d9
Kim Burgestrand <kim@burgestrand.se>
2010-02-24 13:48:31 +0100

    Adds Sprig_Field_Datetime

---
+++ classes/sprig/field/datetime.php
@@ -0,0 +1,33 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+    /**
+     * Datetime Sprig Field
+     *
+     * @author      Kim Burgestrand
+     * @license     X11 License (“MIT”)
+     */
+    class Sprig_Field_Datetime extends Sprig_Field
+    {
+        public $format = 'Y-m-d H:i:s';
+        public $default = NULL;
+        
+        /**
+         * Returns the fields’ value in local timezone
+         * 
+         * @param value
+         * @return time
+         */
+        public function value($value)
+        {
+            $value = parent::value($value);
+            
+            if (is_null($value))
+            {
+                return gmdate('Y-m-d H:i:s');
+            }
+            
+            return date($this->format, strtotime($value . ' +0000'));
+        }
+    }
+    
+/* End of file datetime.php */
+/* Location: ./application/classes/sprig/field/datetime.php */ 
